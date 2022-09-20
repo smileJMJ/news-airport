8318981444f4f60f618b420fbb03bdc4254cbbc4
smileJMJ <mjj0324@gmail.com>
2022-09-20 23:02:20 +0900

    푸터 추가

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,23 @@
   <div class="wrap">
     <Header :type="headerType" />
     <router-view/>
+    <Footer />
   </div>
 </template>
 <script lang="ts" setup>
   import { computed, onMounted } from 'vue';
   import Header from '@/components/Header.vue';
+  import Footer from '@/components/Footer.vue';
   import { useUiStore } from '@/store/uiStore';
   import { HeaderType } from '@/type';
 
   const store = useUiStore();
   const headerType = computed<HeaderType>(() => store.headerType); // 번역 기능 사용을 위해 header는 상태변화가 없도록 router 바깥에 위치시킴
 </script>
+<style lang="scss" scoped>
+  .wrap {
+    position: relative;
+    min-height: 100vh;
+    padding-bottom: 80px;
+  }
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,55 @@
+<template>
+    <footer>
+        <p><a href="https://news-airtport.pocopocori.com/" class="btn-icon site" target="_blank">NewsAirport Web</a></p>
+        <p><a href="mailto:dev.pocori@gmail.com" class="btn-icon email">dev.pocori@gmail.com</a></p>
+    </footer>
+</template>
+<style lang="scss" scoped>
+    footer {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        position: absolute;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        height: 80px;
+        padding: 15px;
+        background: #333;
+
+        p:first-child {
+            position: relative;
+            padding-right: 6px;
+            margin-right: 6px;
+
+            &::after {
+                display: block;
+                content: '';
+                position: absolute;
+                right: 0;
+                top: 2px;
+                width: 1px;
+                height: 13px;
+                background: #f5f5f5;
+            }
+        }
+
+        a {
+            font-size: 12px;
+            color: #f5f5f5;
+            line-height: 1.4;
+
+            &::before {
+                margin-right: 5px;
+            }
+
+            &.site::before {
+                content: '\e852';
+            }
+
+            &.email::before {
+                content: '\e818';
+            }
+        }
+    }
+</style>
